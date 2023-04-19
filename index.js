6c93cf76a4aa929508541d41a44dde710645da6a
ikitsmark <ikitsmark@gmail.com>
2023-04-19 11:10:00 +0200

    valami

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-function addNumbers(a) {
-  return a
+const addNumbers = (a,b) => {
+  return 2
+}
+
+function mulNumbers(a) {
+  return 2 * 8
 }
 
 console.log(addNumbers(5))
+console.log(addNumbers(100, 40))
